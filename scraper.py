4ccf271d745842bf338d9e2df294ffa78d6b2440
Sarthak Rout <sarthak2024@gmail.com>
2020-04-03 01:04:39 +0530

    Added options

--- scraper.py
+++ scraper.py
@@ -1,30 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://summerofcode.withgoogle.com/archive/2019/projects/"
+def scrape(URL, f):
+	r = requests.get(URL)
+	soup = BeautifulSoup(r.content, 'html5lib')
+	table = soup.find_all('div', attrs = {'class':'archive-project-card__header'}) 
+	#print(table)
+	for row in table:
+		project = ''
+		name = row.h4.text[5:]
+		name = ' '.join(name.split())
+		org = row.find_all('div')[1].text
+		org = org[14:]
+		project = name + ',' + org + ',' + row.div.text + '\n'
+		f.write(project)
+
+
+URLgiven = "https://summerofcode.withgoogle.com/archive/2019/projects/"
 print("Give an URL to scrape!")
-URL = input()
+URLgiven = input()
+print("If you want to scrape the whole 12 pages, say 'y' else 'n' ? ")
+options = input()
 filename = 'projects.csv'
-
-r = requests.get(URL)
-
-soup = BeautifulSoup(r.content, 'html5lib')
-
-table = soup.find_all('div', attrs = {'class':'archive-project-card__header'}) 
-
-#print(table)
-
 
 f = open(filename, 'w', encoding = "utf-8")
 
-for row in table:
-	project = ''
-	name = row.h4.text[5:]
-	name = ' '.join(name.split())
-	org = row.find_all('div')[1].text
-	org = org[14:]
-	project = name + ',' + org + ',' + row.div.text + '\n'
-	f.write(project)
+if options == 'y':
+	for i in range(1,13):
+		s = URLgiven + "?page=" + str(i)
+		scrape(s, f)
+else:
+	scrape(URLgiven, f)
 
 print("Scraping is Complete!")
+
 f.close()
+
+
+
